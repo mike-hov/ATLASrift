c606c0f4002f98dbb47089a0a18854eaa5fa174c
mike-hov <mhovdesven@gwu.edu>
2017-12-15 18:59:11 +0100

    FinalChanges

--- Source/ATLASrift/Event.cpp
+++ Source/ATLASrift/Event.cpp
@@ -33,12 +33,12 @@ AEvent::AEvent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitia
 	meshY = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("GeneratedMesh - meshY"));
 	meshCluster = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("GeneratedMesh - meshCluster"));
 
-	int32 temp[] = { 0, 2, 1, 0, 3, 2,
-		4, 5, 6, 4, 6, 7,
-		0, 5, 4, 0, 1, 5,
-		0, 4, 7, 0, 7, 3,
-		1, 6, 5, 1, 2, 6,
-		2, 7, 6, 2, 3, 7 };
+	int32 temp[] = { 0, 1, 2, 0, 2, 3,
+		4, 6, 5, 4, 7, 6,
+		0, 4, 5, 0, 5, 1,
+		0, 7, 4, 0, 3, 7,
+		1, 5, 6, 1, 6, 2,
+		2, 6, 7, 2, 7, 3 };
 
 	for (int i = 0; i < 36; i++)
 		VertexPattern.Add(temp[i]);
